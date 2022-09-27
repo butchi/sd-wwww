bde46e6058637e37492cc1c8eeb12c2fe6cc10ab
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-09-28 06:34:44 +0900

    app実装

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-navigation-drawer app>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="blue">
+      <v-app-bar-title class="text-white">
+        いつどこで誰が何をした Stable Diffusion デモ
+      </v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
